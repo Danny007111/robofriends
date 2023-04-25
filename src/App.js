6012798c20c780ba665c2236b6905ce6a14c1e79
Danny Rodriguez <danny007111@hotmail.com>
2023-04-25 16:10:12 -0500

    Making changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
-import CardList from "./CardList";
+import React, { Component } from 'react';
+import CardList from './CardList';
 import { robots } from './robots'; 
 import SearchBox from './SearchBox'
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>Robofriends</h1>
-            <SearchBox />
-            <CardList robots = {robots}/>
-        </div>
-    );
-}
 
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    // Every time search changes it (logs) -puts- out an EVENT
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+
+        
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase)
+        })
+
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots = {filteredRobots}/>
+            </div>
+        );
+    }
+    
+}
+// to make the page interactive we need to join searchbox and cardlist connect togeether, USING STATE... but where? ... in APP!!!
+// Remember State is parent of props (STATE >> props)
 export default App;
