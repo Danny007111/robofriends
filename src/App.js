048eb18c35b9977f92392e6b42635b71a807c682
Danny Rodriguez <danny007111@hotmail.com>
2023-05-04 19:48:42 -0500

    Adding loading... when no robots are available

--- src/App.js
+++ src/App.js
@@ -42,13 +42,18 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
 
-        return (
-            <div className='tc'>
-                <h1 className='f2'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots = {filteredRobots}/>
-            </div>
-        );
+        // adding loading if robots === 0
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots = {filteredRobots}/>
+                </div>
+            );
+        }
     }
     
 }
